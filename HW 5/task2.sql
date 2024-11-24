12df11f2f411d9346b19e3c8a39e291226f9a336
ileysabokar <ileys.abokar@city.ac.uk>
2024-11-24 16:42:42 +0000

    updatetask2sql

--- HW 5/task2.sql
+++ HW 5/task2.sql
@@ -1,18 +1,22 @@
 -- 1
+
 SELECT SUM(bill_total) AS Income
 FROM restBill;
 
 -- 2
+
 SELECT SUM(bill_total) AS "Feb Income"
 FROM restBill
 WHERE bill_date BETWEEN 160201 AND 160229;
 
 -- 3
+
 SELECT AVG(bill_total) AS AverageBill
 FROM restBill
 WHERE table_no = 2;
 
 -- 4
+
 SELECT 
     MIN(no_of_seats) AS Min,
     MAX(no_of_seats) AS Max,
@@ -21,6 +25,7 @@ FROM restRest_table
 WHERE room_name = 'Blue';
 
 -- 5
+
 SELECT COUNT(DISTINCT table_no) AS DistinctTables
 FROM restBill
 WHERE waiter_no IN (004, 002);
